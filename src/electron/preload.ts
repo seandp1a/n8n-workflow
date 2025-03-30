44c6a15b5d6d33083eecb6afd8546bf2a5eee2b3
sean <seandp1a@gmail.com>
2025-03-30 19:40:49 +0800

    build: init (react + vite + electron + typeScript)

---
+++ src/electron/preload.ts
@@ -0,0 +1,8 @@
+import { contextBridge, ipcRenderer } from "electron";
+
+export const backend = {
+  nodeVersion: async (msg: string): Promise<string> =>
+    await ipcRenderer.invoke("node-version", msg),
+};
+
+contextBridge.exposeInMainWorld("backend", backend);
