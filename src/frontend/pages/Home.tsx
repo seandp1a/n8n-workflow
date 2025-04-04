3db1c8a28cf1d9bcd7f34c094fe50421bd2cc193
sean <seandp1a@gmail.com>
2025-04-04 17:20:31 +0800

    feat: 串接 localStorage 紀錄 token 配置
     fix : <Typography> 補上 component="div" variant="body1" - 修正 react-dom 報錯如 <fieldset> cannot be a descendant of <p>

--- src/frontend/pages/Home.tsx
+++ src/frontend/pages/Home.tsx
@@ -1,34 +1,54 @@
-import { FormControl, TextField } from "@mui/material";
+import { Button, FormControl, TextField } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const fields = [
-  { label: "Jira Token", value: '' },
-  { label: "Conflunce Token", value: '' },
-  { label: "Git Lab Token", value: '' },
-]
+  { label: "Jira Token", value: "" },
+  { label: "Conflunce Token", value: "" },
+  { label: "Git Lab Token", value: "" },
+];
 
 export default function Home() {
   const [fieldValues, setFieldValues] = useState<{ [key: string]: string }>({
-    'Jira Token': '',
-    'Confluence Token': '',
-    'Git Lab Token': '',
+    "Jira Token": "",
+    "Confluence Token": "",
+    "Git Lab Token": "",
   });
 
   const handleInputChange = (label: string, value: string) => {
     setFieldValues({ ...fieldValues, [label]: value });
   };
 
+  useEffect(() => {
+    const oldSetting = localStorage.getItem("oldSetting");
+
+    if (oldSetting) {
+      const parsedSetting = JSON.parse(oldSetting);
+      setFieldValues((prevValues) => ({
+        ...prevValues,
+        ...parsedSetting,
+      }));
+    }
+  }, []);
+
+  function handleSave() {
+    const newSetting = JSON.stringify(fieldValues);
+    localStorage.setItem("oldSetting", newSetting);
+  }
+
   return (
-    <Typography color="grey.800">
+    <Typography color="grey.800" component="div" variant="body1">
       <form
         noValidate
         autoComplete="off"
       >
         {fields.map((field) => (
-          <FormControl key={field.label} sx={{ width: "100%", marginBottom: '1rem' }}>
+          <FormControl
+            key={field.label}
+            sx={{ width: "100%", marginBottom: "1rem" }}
+          >
             <TextField
-              id={`filled-basic-${field.label.toLowerCase().replace(/\s/g, '-')}`}
+              id={`filled-basic-${field.label.toLowerCase().replace(/\s/g, "-")}`}
               label={field.label}
               variant="outlined"
               value={fieldValues[field.label]}
@@ -37,6 +57,12 @@ export default function Home() {
           </FormControl>
         ))}
       </form>
+      <Button
+        variant="outlined"
+        onClick={() => handleSave()}
+      >
+        Save
+      </Button>
     </Typography>
   );
 }
--- src/frontend/pages/Page2.tsx
+++ src/frontend/pages/Page2.tsx
@@ -4,6 +4,8 @@ export default function Page2() {
   return (
     <Typography
       color="grey.800"
+      component="div"
+      variant="body1"
     >
       This is page 222!
     </Typography>
