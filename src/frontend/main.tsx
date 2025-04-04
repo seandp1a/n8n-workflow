8c55c4c6dd8e775e3630951a0fc3d38070b054af
sean <seandp1a@gmail.com>
2025-04-04 15:33:57 +0800

    fix: 修正打包後應用無法使用問題
    - 如果不用 hashRouter，build出來的應用會被告知"Error: No route matches URL "/C:/Users/seanttc/Desktop/react/n8n-workflow/out/react-ts-win32-x64/resources/app.asar/dist/index.html"

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
 import App from "./App.tsx";
 import "./index.css";
 import Layout from "./layouts/dashboard.tsx";
 import Home from "./pages/Home.tsx";
 import Page2 from "./pages/Page2.tsx";
-
-const router = createBrowserRouter([
+// 如果不用 hashRouter，build出來的應用會被告知"Error: No route matches URL "...../n8n-workflow/out/react-ts-win32-x64/resources/app.asar/dist/index.html"
+const router = createHashRouter([
   {
     Component: App,
     children: [
